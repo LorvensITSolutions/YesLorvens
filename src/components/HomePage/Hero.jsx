3bd28a4f46bd10b6ae882904687f0ba22362de00
G-ESWARBHAI <eswargeddam1432@gmail.com>
2025-12-08 15:24:36 +0530

    scroll view modified

--- src/components/HomePage/Hero.jsx
+++ src/components/HomePage/Hero.jsx
@@ -64,7 +64,7 @@ const Hero = () => {
 
       {/* Mobile background */}
       <div
-        className="md:hidden absolute inset-0 w-full h-[100vh]"
+        className="md:hidden absolute inset-0 w-full h-[70vh]"
         style={{
           backgroundColor: "#111827",
           backgroundImage: `url(${mobileBg})`,
